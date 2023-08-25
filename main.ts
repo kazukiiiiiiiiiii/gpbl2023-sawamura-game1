3fa29d1dc9d089c01f41488c324dd75fe330fe37
kazukiiiiiiiiiii <143159322+kazukiiiiiiiiiii@users.noreply.github.com>
2023-08-25 17:05:40 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,47 @@
-radio.setGroup(39)
-radio.sendNumber(1)
-basic.forever(function () {
+radio.onReceivedNumber(function (receivedNumber) {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showString("GAMEOVER")
+        basic.pause(10500)
+    } else if (input.buttonIsPressed(Button.B)) {
+        basic.showString("GAMEOVER")
+        basic.pause(10500)
+    } else if (input.buttonIsPressed(Button.AB)) {
+        basic.showString("GAMEOVER")
+        basic.pause(10500)
+    } else {
+    	
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    y = y - 1
+})
+radio.onReceivedString(function (receivedString) {
 	
+})
+input.onButtonPressed(Button.B, function () {
+    y = y + 1
+})
+radio.onReceivedValue(function (name, value) {
+	
+})
+let y = 0
+let judge = 0
+radio.setGroup(39)
+let x = 2
+y = 4
+basic.forever(function () {
+    basic.clearScreen()
+    led.plot(x, y)
+    if (x < 0) {
+        x = 0
+    }
+    if (x > 4) {
+        x = 4
+    }
+    if (y < 0) {
+        y = 0
+    }
+    if (y > 4) {
+        y = 4
+    }
 })
